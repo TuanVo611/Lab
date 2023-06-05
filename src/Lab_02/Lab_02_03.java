cebd0a9c53196f865805edfc6383e635f7a94048
TuanVA <vo.anhtuan.0611@gmail.com>
2023-06-05 21:04:30 +0700

    Lab_02_03

--- src/Lab_02/Lab_02_03.java
+++ src/Lab_02/Lab_02_03.java
@@ -1,2 +1,27 @@
-package Lab_02;public class Lab_02_03 {
+package Lab_02;
+
+import java.util.Scanner;
+
+public class Lab_02_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input your height: ");
+        float height = scanner.nextFloat();
+        System.out.print("Input your weight: ");
+        float weight = scanner.nextFloat();
+        float BMI = weight / (height * 2);
+        System.out.println(BMI);
+
+        if (BMI < 18.5) {
+            System.out.println("Underweight");
+            double weightIncrease = (18.5 - BMI) * height * 2;
+            System.out.println("You should increase " + weightIncrease + " kg");
+        } else if (BMI <= 24.9) {
+            System.out.println("Normal weight");
+        } else if (BMI > 25 && BMI < 29.9) {
+            System.out.println("Overweight");
+        } else {
+            System.out.println("Obesity");
+        }
+    }
 }
