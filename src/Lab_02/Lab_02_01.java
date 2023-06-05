46172e854b9420f7c55f2f0b52e19c97422ba10f
TuanVA <vo.anhtuan.0611@gmail.com>
2023-06-05 20:30:14 +0700

    Lab 02

---
+++ src/Lab_02/Lab_02_01.java
@@ -0,0 +1,24 @@
+package Lab_02;
+
+import java.util.Scanner;
+
+public class Lab_02_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input your height: ");
+        float height = scanner.nextFloat();
+        System.out.print("Input your weight: ");
+        float weight = scanner.nextFloat();
+        float BMI = weight / (height * 2);
+
+        if (BMI < 18.5) {
+            System.out.println("Underweight");
+        } else if (BMI <= 24.9) {
+            System.out.println("Normal weight");
+        } else if (BMI > 25 && BMI < 29.9) {
+            System.out.println("Overweight");
+        } else {
+            System.out.println("Obesity");
+        }
+    }
+}
---
+++ src/Lab_02/Lab_02_02.java
@@ -0,0 +1,17 @@
+package Lab_02;
+
+import java.util.Scanner;
+
+public class Lab_02_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input your number: ");
+        int numberUserInput = scanner.nextInt();
+
+        if (numberUserInput % 2 == 0) {
+            System.out.println("Number user input is even number!");
+        } else {
+            System.out.println("Number user input is odd number!");
+        }
+    }
+}
